d0581090898f8ad790d6feeb37f18b5cd531ee94
Jasper King-Harman <j.kingharman@googlemail.com>
2018-10-28 13:12:06 +0000

    Namespace classes

--- lib/tweet_delete/deletion_recorder.rb
+++ lib/tweet_delete/deletion_recorder.rb
@@ -1,55 +1,57 @@
-# A class that tracks what tweets have been deleted - useful because you may
-# need to resume the bulk deletion if it errors. The class updates a JSON
-# representation of tweets on deletion. It reads in that representation to
-# find the next batch to delete.
-class DeletionRecorder
-  PATH_TO_TWEETS = '../tweets.json'.freeze
-  BATCH_SIZE = 15
-  def initialize(file: File)
-    @file = file
+module TweetDelete
+  # A class that tracks what tweets have been deleted - useful because you may
+  # need to resume the bulk deletion if it errors. The class updates a JSON
+  # representation of tweets on deletion. It reads in that representation to
+  # find the next batch to delete.
+  class DeletionRecorder
+    PATH_TO_TWEETS = '../tweets.json'.freeze
+    BATCH_SIZE = 15
+    def initialize(file: File)
+      @file = file
+    end
+
+    def to_delete
+      file = read_tweets
+      validate_tweets(file)
+      tweets_to_delete
+    end
+
+    def record_deletes(ids)
+      update_deleted_tweets(ids)
+      write_tweets
+    end
+
+    private
+
+    def read_tweets
+      puts 'Reading archived tweets..'
+      File.read(PATH_TO_TWEETS)
+    rescue Errno::ENOENT => e
+      abort "Unable to read file: #{e}"
+    end
+
+    def validate_tweets(file)
+      self.tweets = JSON.parse(file)
+    rescue JSON::ParserError => e
+      abort "Unable to parse tweets: #{e}"
+    end
+
+    # Returns undeleted tweet IDs.
+    def tweets_to_delete
+      tweets.keys.reject { |k| k =~ /deleted/ }.first(BATCH_SIZE).map(&:to_i)
+    end
+
+    # Mungs tweet IDs, marking them as deleted.
+    def update_deleted_tweets(ids)
+      puts "Recording tweets #{ids} as deleted"
+      ids.each { |id| tweets["deleted_#{id}"] = tweets.delete id.to_s }
+    end
+
+    def write_tweets
+      File.write(PATH_TO_TWEETS, tweets.to_json)
+    end
+
+    attr_reader :file
+    attr_accessor :tweets
   end
-
-  def to_delete
-    file = read_tweets
-    validate_tweets(file)
-    tweets_to_delete
-  end
-
-  def record_deletes(ids)
-    update_deleted_tweets(ids)
-    write_tweets
-  end
-
-  private
-
-  def read_tweets
-    puts 'Reading archived tweets..'
-    File.read(PATH_TO_TWEETS)
-  rescue Errno::ENOENT => e
-    abort "Unable to read file: #{e}"
-  end
-
-  def validate_tweets(file)
-    self.tweets = JSON.parse(file)
-  rescue JSON::ParserError => e
-    abort "Unable to parse tweets: #{e}"
-  end
-
-  # Returns undeleted tweet IDs.
-  def tweets_to_delete
-    tweets.keys.reject { |k| k =~ /deleted/ }.first(BATCH_SIZE).map(&:to_i)
-  end
-
-  # Mungs tweet IDs, marking them as deleted.
-  def update_deleted_tweets(ids)
-    puts "Recording tweets #{ids} as deleted"
-    ids.each { |id| tweets["deleted_#{id}"] = tweets.delete id.to_s }
-  end
-
-  def write_tweets
-    File.write(PATH_TO_TWEETS, tweets.to_json)
-  end
-
-  attr_reader :file
-  attr_accessor :tweets
 end
--- lib/tweet_delete/tweet_deleter.rb
+++ lib/tweet_delete/tweet_deleter.rb
@@ -1,35 +1,37 @@
-# A class for requesting Twitter to destroy specified tweets.
-class TweetDeleter
-  def self.client
-    @client ||= Twitter::REST::Client.new do |config|
-      config.consumer_key = ENV['CONSUMER_KEY']
-      config.consumer_secret     = ENV['CONSUMER_SECRET']
-      config.access_token        = ENV['ACCESS_TOKEN']
-      config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+module TweetDelete
+  # A class for requesting Twitter to destroy specified tweets.
+  class TweetDeleter
+    def self.client
+      @client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key = ENV['CONSUMER_KEY']
+        config.consumer_secret     = ENV['CONSUMER_SECRET']
+        config.access_token        = ENV['ACCESS_TOKEN']
+        config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+      end
     end
-  end
 
-  def initialize
-    @retry = 0
-  end
+    def initialize
+      @retry = 0
+    end
 
-  def call(id)
-    TweetDeleter.client.destroy_status(id)
-  rescue Twitter::Error::NotFound => e
-    puts "Tweet #{id} not found"
-  rescue HTTP::ConnectionError => e
-    abort 'Connection error. Restart the process'
-  rescue Twitter::Error::TooManyRequests => e
-    puts 'Rate limit exceeded.'
-    sleep e.rate_limit.reset_in + 1
-    increase_retry_count
-    retry
-  end
+    def call(id)
+      TweetDeleter.client.destroy_status(id)
+    rescue Twitter::Error::NotFound => e
+      puts "Tweet #{id} not found"
+    rescue HTTP::ConnectionError => e
+      abort 'Connection error. Restart the process'
+    rescue Twitter::Error::TooManyRequests => e
+      puts 'Rate limit exceeded.'
+      sleep e.rate_limit.reset_in + 1
+      increase_retry_count
+      retry
+    end
 
-  private
+    private
 
-  def increase_retry_count
-    self.retries += 1
-    abort 'At risk of blacklisting' if retries == 4
+    def increase_retry_count
+      self.retries += 1
+      abort 'At risk of blacklisting' if retries == 4
+    end
   end
 end
