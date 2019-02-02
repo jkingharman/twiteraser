32504fa16943dc6c8e77a6c596c5a02652894385
Jasper King-Harman <j.kingharman@googlemail.com>
2019-02-02 15:35:51 +0000

    Include rep tweets

---
+++ tweets.js
@@ -0,0 +1,67 @@
+[ {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "780116105515851778",
+  "text" : "Third",
+  "id" : 1091719461483229186,
+  "created_at" : "2019-02-02 15:26:17 +0000",
+  "user" : {
+    "name" : "_jkh_",
+    "screen_name" : "jkh18548343",
+    "protected" : false,
+    "id_str" : "1053724371003551744",
+    "profile_image_url_https" : "https:\/\/abs.twimg.com\/sticky\/default_profile_images\/default_profile_normal.png",
+    "id" : 1053724371003551744,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "777557209458831361",
+  "text" : "Second",
+  "id" : 1091719443221237762,
+  "created_at" : "2019-02-02 15:26:13 +0000",
+  "user" : {
+    "name" : "_jkh_",
+    "screen_name" : "jkh18548343",
+    "protected" : false,
+    "id_str" : "1053724371003551744",
+    "profile_image_url_https" : "https:\/\/abs.twimg.com\/sticky\/default_profile_images\/default_profile_normal.png",
+    "id" : 1053724371003551744,
+    "verified" : false
+  }
+}, {
+  "source" : "\u003Ca href=\"http:\/\/twitter.com\" rel=\"nofollow\"\u003ETwitter Web Client\u003C\/a\u003E",
+  "entities" : {
+    "user_mentions" : [ ],
+    "media" : [ ],
+    "hashtags" : [ ],
+    "urls" : [ ]
+  },
+  "geo" : { },
+  "id_str" : "766688604068413440",
+  "text" : "Test",
+  "id" : 1091719388523347975,
+  "created_at" : "2019-02-02 15:26:00 +0000",
+  "user" : {
+    "name" : "_jkh_",
+    "screen_name" : "jkh18548343",
+    "protected" : false,
+    "id_str" : "1053724371003551744",
+    "profile_image_url_https" : "https:\/\/abs.twimg.com\/sticky\/default_profile_images\/default_profile_normal.png",
+    "id" : 1053724371003551744,
+    "verified" : false
+  }
+} ]
